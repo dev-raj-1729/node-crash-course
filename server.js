c6cff59fb7387e3cf34bcf2c3da00d3daaadfbfb
dev-raj-1729 <dev.raj.r1729@gmail.com>
2022-09-30 19:54:08 +0530

    added home page

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     console.log('request made');
     res.setHeader('Content-Type','text/html');
-    res.end('<p>Hello World</p>');
+    
+    path = './views/index.html';
+
+    fs.readFile(path,(err,data)=>{
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+        console.log('Page Send');
+    });
+
 });
 
 server.listen(3000,'localhost',()=>{
