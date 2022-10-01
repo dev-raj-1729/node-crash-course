cf2d421d539ddfe07a892dca77466887b66a1b2d
dev-raj-1729 <dev.raj.r1729@gmail.com>
2022-10-01 16:07:00 +0530

    added morgan middleware to log

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
-
+const morgan = require('morgan');
 
 const app = express();
 
 app.listen(3000);
+
+app.use(morgan('dev'));
 
 app.set('view engine','ejs');
 
