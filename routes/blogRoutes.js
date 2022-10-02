4737f7ac1cd19d59a4d198a077a5c2b2e658d30d
dev-raj-1729 <dev.raj.r1729@gmail.com>
2022-10-02 18:55:59 +0530

    refactored controllers intro separate file

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,49 +1,13 @@
 const express = require('express');
 
-const Blog = require('../models/blogs');
+const blogController = require('../controllers/blogController');
 
 router = express.Router();
 
-router.get('/',(req,res)=> {
-    Blog.find()
-    .then((blogs)=>{
-        res.render('index',{title:'Home',blogs});
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
-})
-
-router.post('/',(req,res)=>{
-    const blog = new Blog(req.body);
-    blog.save().then((result)=>{
-        res.redirect('/blogs');
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
-})
-
-router.get('/create',(req,res)=> {
-    res.render('create',{title:'New Blog'});
-})
-
-router.get('/:id',(req,res)=>{
-    Blog.findById(req.params.id)
-    .then((blog)=>{
-        res.render('details',{title:'Details',blog});
-    }).catch((err)=>{
-        console.log(err);
-    });
-})
-
-router.delete('/:id',(req,res)=>{
-    Blog.findByIdAndDelete(req.params.id)
-    .then((blog)=>{
-        res.json({redirect:'/blogs'});
-    }).catch((err)=>{
-        console.log(err);
-    });
-})
+router.get('/',blogController.blog_index)
+router.post('/',blogController.blog_create_post)
+router.get('/create',blogController.blog_create_get)
+router.get('/:id',blogController.blog_details)
+router.delete('/:id',blogController.blog_delete)
 
 module.exports = router;
